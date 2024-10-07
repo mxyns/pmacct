8136f923e31a48ef7609b113c77c92cb38082c26
Maxence Younsi <mx.yns@outlook.fr>
2024-10-07 02:57:31 +0200

    quote values in scripts

--- test-framework/tools/pmacct_build/build_docker_images.sh
+++ test-framework/tools/pmacct_build/build_docker_images.sh
@@ -18,17 +18,17 @@ if "$PMACCT_ROOT_LOCATION/config.status" --config | grep -q "enable-pmacct-gauze
     exit 1
   fi
 
-  docker build --build-arg NUM_WORKERS=$(nproc) -t base:$TAG -f $PMACCT_ROOT_LOCATION/docker/pmacct-gauze-base/Dockerfile $PMACCT_ROOT_LOCATION || exit $?
+  docker build --build-arg NUM_WORKERS="$(nproc)" -t base:$TAG -f "$PMACCT_ROOT_LOCATION/docker/pmacct-gauze-base/Dockerfile" "$PMACCT_ROOT_LOCATION" || exit $?
 else
 
   echo "Building pmacct docker images"
-  docker build --build-arg NUM_WORKERS=$(nproc) -t base:$TAG -f $PMACCT_ROOT_LOCATION/docker/base/Dockerfile $PMACCT_ROOT_LOCATION || exit $?
+  docker build --build-arg NUM_WORKERS="$(nproc)" -t base:$TAG -f "$PMACCT_ROOT_LOCATION/docker/base/Dockerfile" "$PMACCT_ROOT_LOCATION" || exit $?
 fi
 
-docker build --build-arg NUM_WORKERS=$(nproc) -t nfacctd:$TAG -f $PMACCT_ROOT_LOCATION/docker/nfacctd/Dockerfile $PMACCT_ROOT_LOCATION || exit $?
-docker build --build-arg NUM_WORKERS=$(nproc) -t pmacctd:$TAG -f $PMACCT_ROOT_LOCATION/docker/pmacctd/Dockerfile $PMACCT_ROOT_LOCATION || exit $?
-docker build --build-arg NUM_WORKERS=$(nproc) -t pmbgpd:$TAG -f $PMACCT_ROOT_LOCATION/docker/pmbgpd/Dockerfile $PMACCT_ROOT_LOCATION || exit $?
-docker build --build-arg NUM_WORKERS=$(nproc) -t pmbmpd:$TAG -f $PMACCT_ROOT_LOCATION/docker/pmbmpd/Dockerfile $PMACCT_ROOT_LOCATION || exit $?
-docker build --build-arg NUM_WORKERS=$(nproc) -t pmtelemetryd:$TAG -f $PMACCT_ROOT_LOCATION/docker/pmtelemetryd/Dockerfile $PMACCT_ROOT_LOCATION || exit $?
-docker build --build-arg NUM_WORKERS=$(nproc) -t sfacctd:$TAG -f $PMACCT_ROOT_LOCATION/docker/sfacctd/Dockerfile $PMACCT_ROOT_LOCATION || exit $?
-docker build --build-arg NUM_WORKERS=$(nproc) -t uacctd:$TAG -f $PMACCT_ROOT_LOCATION/docker/uacctd/Dockerfile $PMACCT_ROOT_LOCATION || exit $?
+docker build --build-arg NUM_WORKERS="$(nproc)" -t nfacctd:$TAG -f "$PMACCT_ROOT_LOCATION/docker/nfacctd/Dockerfile" "$PMACCT_ROOT_LOCATION" || exit $?
+docker build --build-arg NUM_WORKERS="$(nproc)" -t pmacctd:$TAG -f "$PMACCT_ROOT_LOCATION/docker/pmacctd/Dockerfile" "$PMACCT_ROOT_LOCATION" || exit $?
+docker build --build-arg NUM_WORKERS="$(nproc)" -t pmbgpd:$TAG -f "$PMACCT_ROOT_LOCATION/docker/pmbgpd/Dockerfile" "$PMACCT_ROOT_LOCATION" || exit $?
+docker build --build-arg NUM_WORKERS="$(nproc)" -t pmbmpd:$TAG -f "$PMACCT_ROOT_LOCATION/docker/pmbmpd/Dockerfile" "$PMACCT_ROOT_LOCATION" || exit $?
+docker build --build-arg NUM_WORKERS="$(nproc)" -t pmtelemetryd:$TAG -f "$PMACCT_ROOT_LOCATION/docker/pmtelemetryd/Dockerfile" "$PMACCT_ROOT_LOCATION" || exit $?
+docker build --build-arg NUM_WORKERS="$(nproc)" -t sfacctd:$TAG -f "$PMACCT_ROOT_LOCATION/docker/sfacctd/Dockerfile" "$PMACCT_ROOT_LOCATION" || exit $?
+docker build --build-arg NUM_WORKERS="$(nproc)" -t uacctd:$TAG -f "$PMACCT_ROOT_LOCATION/docker/uacctd/Dockerfile" "$PMACCT_ROOT_LOCATION" || exit $?
