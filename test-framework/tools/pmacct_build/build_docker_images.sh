a4e5f90fdead49c5421157fe6cc3330d99b9a52b
Pedro-W21 <89784517+Pedro-W21@users.noreply.github.com>
2025-10-10 21:13:48 +0200

    fixing the BGP rebase issues and adding the temporary docker building script modification so it can't be lost

--- test-framework/tools/pmacct_build/build_docker_images.sh
+++ test-framework/tools/pmacct_build/build_docker_images.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
+set -x
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "$0" )" &> /dev/null && pwd )
 PMACCT_ROOT_LOCATION="$SCRIPT_DIR/../../../"
 
 TAG='_build'
 
-# TODO add script parameter to build with pmacct-gauze too
-if "$PMACCT_ROOT_LOCATION/config.status" --config | grep -q "enable-pmacct-gauze"; then
-
-  echo "Building pmacct docker images with pmacct-gauze"
-  PMACCT_GAUZE_ROOT_LOCATION=$(realpath "$PMACCT_ROOT_LOCATION/docker")
-  if [ ! -d "$PMACCT_GAUZE_ROOT_LOCATION/pmacct-gauze" ]; then
-    echo "Error: $PMACCT_GAUZE_ROOT_LOCATION/pmacct-gauze does not exist."
-    echo
-    echo "     !!! Please clone the pmacct repo in $PMACCT_GAUZE_ROOT_LOCATION !!!"
-    echo
-    exit 1
-  fi
-
-  docker build --build-arg NUM_WORKERS="$(nproc)" -t base:$TAG -f "$PMACCT_ROOT_LOCATION/docker/pmacct-gauze-base/Dockerfile" "$PMACCT_ROOT_LOCATION" || exit $?
-else
-
-  echo "Building pmacct docker images"
-  docker build --build-arg NUM_WORKERS="$(nproc)" -t base:$TAG -f "$PMACCT_ROOT_LOCATION/docker/base/Dockerfile" "$PMACCT_ROOT_LOCATION" || exit $?
+echo "Building pmacct docker images with pmacct-gauze"
+PMACCT_GAUZE_ROOT_LOCATION=$(realpath "$PMACCT_ROOT_LOCATION/docker")
+if [ ! -d "$PMACCT_GAUZE_ROOT_LOCATION/pmacct-gauze" ]; then
+  echo "Error: $PMACCT_GAUZE_ROOT_LOCATION/pmacct-gauze does not exist."
+  echo
+  echo "     !!! Please clone the pmacct repo in $PMACCT_GAUZE_ROOT_LOCATION !!!"
+  echo
+  exit 1
 fi
+
+docker build --build-arg NUM_WORKERS="$(nproc)" -t base:$TAG -f "$PMACCT_ROOT_LOCATION/docker/pmacct-gauze-base/Dockerfile" "$PMACCT_ROOT_LOCATION" || exit $?
+
+echo "Building pmacct docker images"
+
+# classic pmaccct build
+# docker build --build-arg NUM_WORKERS="$(nproc)" -t base:$TAG -f "$PMACCT_ROOT_LOCATION/docker/base/Dockerfile" "$PMACCT_ROOT_LOCATION" || exit $?
+
 
 docker build --build-arg NUM_WORKERS="$(nproc)" -t nfacctd:$TAG -f "$PMACCT_ROOT_LOCATION/docker/nfacctd/Dockerfile" "$PMACCT_ROOT_LOCATION" || exit $?
 docker build --build-arg NUM_WORKERS="$(nproc)" -t pmacctd:$TAG -f "$PMACCT_ROOT_LOCATION/docker/pmacctd/Dockerfile" "$PMACCT_ROOT_LOCATION" || exit $?
