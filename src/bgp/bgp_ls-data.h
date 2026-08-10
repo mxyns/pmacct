325653f213dd1c3dc741e0aeb1467276ef8ca930
Paolo Lucente <pl+github@pmacct.net>
2026-08-10 21:49:14 +0000

    * BGP-LS: added support for Link Name TLV (1098)

--- src/bgp/bgp_ls-data.h
+++ src/bgp/bgp_ls-data.h
@@ -1,6 +1,6 @@
 /*  
     pmacct (Promiscuous mode IP Accounting package)
-    pmacct is Copyright (C) 2003-2025 by Paolo Lucente
+    pmacct is Copyright (C) 2003-2026 by Paolo Lucente
 */
 
 /*
@@ -44,6 +44,7 @@ const struct bgp_ls_attr_tlv_print_list_entry bgp_ls_attr_tlv_print_list[] = {
   {BGP_LS_LINK_MSD, "attr_link_msd", 0, bgp_ls_attr_tlv_msd_print},
   {BGP_LS_ATTR_NODE_FLAG_BITS, "attr_node_flags", BGP_LS_PRINT_HEX, bgp_ls_attr_tlv_int8_print},
   {BGP_LS_ATTR_NODE_NAME, "attr_node_name", 0, bgp_ls_attr_tlv_string_print},
+  {BGP_LS_ATTR_LINK_NAME, "attr_link_name", 0, bgp_ls_attr_tlv_string_print},
   {BGP_LS_ATTR_ISIS_AREA_ID, "attr_isis_area_id", 0, bgp_ls_attr_tlv_isis_areaid_print}, 
   {BGP_LS_ATTR_V4_RID_LOCAL, "attr_v4_rid_local", 0, bgp_ls_attr_tlv_ip_print},
   {BGP_LS_ATTR_V6_RID_LOCAL, "attr_v6_rid_local", 0, bgp_ls_attr_tlv_ip_print},
